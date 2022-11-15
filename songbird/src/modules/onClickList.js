40b789db53b52c3a0eef06bd067ea192d531bbee
MarinaZai <marintos34@gmail.com>
2022-11-15 15:38:53 +0100

    feat:create center right block for quize page

--- songbird/src/modules/onClickList.js
+++ songbird/src/modules/onClickList.js
@@ -1,35 +1,101 @@
 export const onClickList = () => {
   const liItem = document.querySelectorAll(".list-item");
   const audioQuize = document.querySelector(".audio-quize");
-  const imageRightBird = document.querySelector(".top-image")
-  const nameRigthBird = document.querySelector(".top-question-title")
+  const imageRightBird = document.querySelector(".top-image");
+  const nameRigthBird = document.querySelector(".top-question-title");
   const score = document.querySelector(".score");
+  const centerRightBlock = document.querySelector(".center-right");
 
   let scoreResult = 0;
   score.innerHTML = `${scoreResult}`;
-  
+
   [].forEach.call(liItem, function (el) {
-    //console.log(el)
+    //console.log(el);
     el.onclick = function (e) {
       let audioQuizeSrc = audioQuize.getAttribute("src");
       //console.log(audioQuizeSrc);
       let valueLiAudio = e.target.getAttribute("audio");
       //console.log(valueLiAudio);
-      let name = e.target.innerHTML
-      let imageSrc = e.target.getAttribute("image")
-
+      let imageSrc = e.target.getAttribute("image");
+      let name = e.target.innerHTML;
+      let english = e.target.getAttribute("species");
+      let description = e.target.getAttribute("description");
       if (audioQuizeSrc === valueLiAudio) {
         scoreResult = scoreResult + 5;
         score.innerHTML = `${scoreResult}`;
-        imageRightBird.setAttribute("src", `${imageSrc}`)
-        nameRigthBird.innerHTML = `${name}`
+        imageRightBird.setAttribute("src", `${imageSrc}`);
+        nameRigthBird.innerHTML = `${name}`;
         el.style.color = "green";
+
+        const centerRightImage = document.createElement("img");
+        centerRightImage.src = imageSrc;
+        centerRightImage.alt = name;
+        centerRightImage.classList.add("center-right-image");
+
+        const centerRightFullDescription = document.createElement("div");
+        centerRightFullDescription.classList.add("center-right-full-description");
+
+        const russianName = document.createElement("span");
+        russianName.classList.add("russian-name");
+        russianName.innerHTML = `${name}`
+
+        const englishName = document.createElement("span");
+        englishName.classList.add("english-name");
+        englishName.innerHTML = ` (${english})`
+        console.log(englishName)
+
+        const audio = document.createElement("AUDIO");
+        audio.setAttribute("src", `${valueLiAudio}`);
+        audio.setAttribute("controls", "controls");
+
+        const centerRightDescription = document.createElement("div");
+        centerRightDescription.classList.add("enter-right-description");
+        centerRightDescription.innerHTML = `${description}`
+        centerRightBlock.innerHTML = ""
+        centerRightBlock.appendChild(centerRightImage)
+        centerRightBlock.appendChild(centerRightFullDescription)
+        centerRightFullDescription.appendChild(russianName)
+        centerRightFullDescription.appendChild(englishName)
+        centerRightFullDescription.appendChild(audio)
+        centerRightFullDescription.appendChild(centerRightDescription)
+
       } else {
         scoreResult = scoreResult - 1;
-        score.innerHTML = `${scoreResult}`
+        score.innerHTML = `${scoreResult}`;
         el.style.color = "red";
+
+        const centerRightImage = document.createElement("img");
+        centerRightImage.src = imageSrc;
+        centerRightImage.alt = name;
+        centerRightImage.classList.add("center-right-image");
+
+        const centerRightFullDescription = document.createElement("div");
+        centerRightFullDescription.classList.add("center-right-full-description");
+
+        const russianName = document.createElement("span");
+        russianName.classList.add("russian-name");
+        russianName.innerHTML = `${name}`
+
+        const englishName = document.createElement("span");
+        englishName.classList.add("english-name");
+        englishName.innerHTML = ` (${english})`
+        console.log(englishName)
+
+        const audio = document.createElement("AUDIO");
+        audio.setAttribute("src", `${valueLiAudio}`);
+        audio.setAttribute("controls", "controls");
+
+        const centerRightDescription = document.createElement("div");
+        centerRightDescription.classList.add("enter-right-description");
+        centerRightDescription.innerHTML = `${description}`
+        centerRightBlock.innerHTML = ""
+        centerRightBlock.appendChild(centerRightImage)
+        centerRightBlock.appendChild(centerRightFullDescription)
+        centerRightFullDescription.appendChild(russianName)
+        centerRightFullDescription.appendChild(englishName)
+        centerRightFullDescription.appendChild(audio)
+        centerRightFullDescription.appendChild(centerRightDescription)
       }
-      
     };
   });
 };
--- songbird/src/modules/quizeItem.js
+++ songbird/src/modules/quizeItem.js
@@ -7,13 +7,15 @@ export const quizeItem = (array) => {
     return;
   }
   array.forEach((array_item, index) => {
+    //console.log(array_item)
     const listBlock = document.createElement("li");
     listBlock.classList.add("list-item");
     listBlock.innerHTML = array_item.name;
     blokItem.appendChild(listBlock);
 
 listBlock.setAttribute("audio", array_item.audio)
-
+listBlock.setAttribute("species", array_item.species)
+listBlock.setAttribute("description", array_item.description)
 listBlock.setAttribute("image", array_item.image)
     //console.log(listBlock)
     return listBlock;
